72f24869e909c9c5b5992b011a089c324d6a047d
candycoder21 <62893559+candycoder21@users.noreply.github.com>
2020-10-02 23:53:31 +0530

    added sum of complex no.

---
+++ Sum of complex.py
@@ -0,0 +1,7 @@
+a=int(input("Input real part of first complex no. "))
+b=int(input("Input imaginary part of first complex no. "))
+c=int(input("Input real part of second complex no. "))
+d = int(input("Input imaginary part of second complex no. "))
+z1=complex(a,b)
+z2=complex(c,d)
+print("Sum of two complex number ",z1+z2)
